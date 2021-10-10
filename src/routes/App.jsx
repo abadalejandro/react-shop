43f3fa6713e37699ec54ed5ccaf34394fd9a3187
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-10-10 17:00:55 +0200

    refc: added new imports and routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,6 +6,12 @@ import Layout from '../containers/Layout';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 
 const App = () => {
 
@@ -19,7 +25,7 @@ const App = () => {
                     <Route exact path='/recovery-password' component={RecoveryPassword} />
                     <Route exact path='/send-email' component={SendEmail} />
                     <Route exact path='/new-password' component={NewPassword} />
-                    <Route exact path='/account' component={MyAccout} />
+                    <Route exact path='/account' component={MyAccount} />
                     <Route exact path='/signup' component={CreateAccount} />
                     <Route exact path='/checkout' component={Checkout} />
                     <Route exact path='/orders' component={Orders} />
