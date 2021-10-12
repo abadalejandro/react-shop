3971ab0bd20af2c177a90314de4bf7f8a0763a78
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-10-12 09:39:45 +0200

    feat: added custom hook useGetProducts

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,23 +1,20 @@
+import uniqid from 'uniqid';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import ProductItem from '../components/ProductItem';
+import useGetProducts from '../hooks/useGetProducts';
 import '../styles/ProductList.scss';
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, []);
-
-
+	const products = useGetProducts(API);
+	
 	return (
 		<section className="main-container">
 			<div className="ProductList">
 				{
 					products.map(product => (
-						<ProductItem />
+						<ProductItem product={product} key={uniqid()}/>
 					))
 				}
 			</div>
@@ -26,3 +23,5 @@ const ProductList = () => {
 }
 
 export default ProductList;
+
+
