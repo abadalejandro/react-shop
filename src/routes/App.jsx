e9df02edc7e5b75efcb45bcad646159fd1c4b769
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-10-13 08:44:10 +0200

    feat: added some imports

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import '../styles/global.css';
-import Login from '../containers/Login';
+// import '../styles/global.css';
+import Login from '../pages/Login';
 import Layout from '../containers/Layout';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import RecoveryPassword from '../pages/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import SendEmail from '../pages/SendEmail';
@@ -12,28 +12,34 @@ import MyAccount from '../pages/MyAccount';
 import CreateAccount from '../pages/CreateAccount';
 import Checkout from '../pages/Checkout';
 import Orders from '../pages/Orders';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
+import '@styles/global.css';
+import useInititalState from '../hooks/useInitialState';
 
 const App = () => {
-
+    const initialState = useInititalState();
 
     return (
-        <BrowserRouter>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path='/recovery-password' component={RecoveryPassword} />
-                    <Route exact path='/send-email' component={SendEmail} />
-                    <Route exact path='/new-password' component={NewPassword} />
-                    <Route exact path='/account' component={MyAccount} />
-                    <Route exact path='/signup' component={CreateAccount} />
-                    <Route exact path='/checkout' component={Checkout} />
-                    <Route exact path='/orders' component={Orders} />
-                    
-                    <Route path='*' component={NotFound} />
-                </Switch>
-            </Layout>
-        </BrowserRouter>
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path='/login' component={Login} />
+                        <Route exact path='/recovery-password' component={RecoveryPassword} />
+                        <Route exact path='/send-email' component={SendEmail} />
+                        <Route exact path='/new-password' component={NewPassword} />
+                        <Route exact path='/account' component={MyAccount} />
+                        <Route exact path='/signup' component={CreateAccount} />
+                        <Route exact path='/checkout' component={Checkout} />
+                        <Route exact path='/orders' component={Orders} />
+
+                        <Route path='*' component={NotFound} />
+                    </Switch>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
     )
 }
 
